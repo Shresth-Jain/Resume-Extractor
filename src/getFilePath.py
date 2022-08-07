df94ca83e417e6a39158cc67f411c58c0fb0d77f
Shresth-Jain <er.shresthjain@gmail.com>
2022-08-07 16:25:35 +0530

    getFilePath updated

--- src/getFilePath.py
+++ src/getFilePath.py
@@ -1,22 +1,10 @@
 from inspect import getfile
 import tkinter as tk
 
-from tkinter import ttk, filedialog
+from tkinter import filedialog
 from tkinter.filedialog import askopenfile
-import os
 
-# from test import getDirectory
 class App(tk.Tk):
-
-    # def __init__(self):
-    #     tk.Tk.__init__(self) # create window
-    #     self.title("Resume Extractor")
-    #     self.geometry("500x250")
-    #     label = tk.Label(self, text="Click the Button to browse the File", font=('Georgia 13'))
-    #     label.pack(pady=10)
-    #     self.filename = "" # variable to store filename
-    #     tk.Button(self, text='Browse', command=self.openfile).pack()
-    #     self.mainloop()
 
     def getFilePath(self):
         tk.Tk.__init__(self) # create window
@@ -34,7 +22,7 @@ class App(tk.Tk):
         
     def getDirectory(self):
         # get a directory path by user
-        self.directoryName=filepath=filedialog.askdirectory(initialdir=r"F:\python\pythonProject",
+        self.directoryName=filepath=filedialog.askdirectory(initialdir=r"",
                                         title="Dialog box")
         label_path=tk.Label(self,text=filepath,font=('italic 14'))
         label_path.pack(pady=20)
@@ -43,15 +31,15 @@ class App(tk.Tk):
 
     def getDirectoryPath(self):
         self.title("Resume Extractor")
-        self.geometry('400x200')
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_columnconfigure(0, weight = 1)
+        self.geometry("500x250")
+        # self.grid_rowconfigure(0, weight = 1)
+        # self.grid_columnconfigure(0, weight = 1)
         dialog_btn = tk.Button(self, text='Select the Zip File Output Directory', command = self.getDirectory)
-        dialog_btn.pack()
+        dialog_btn.pack(pady=10)
         self.mainloop()
         return(self.directoryName)
 
-    def getPath(self):
+    def getFilePath(self):
         self.title("Resume Extractor")
         self.geometry("500x250")
         label = tk.Label(self, text="Click the Button to browse the CSV File", font=('Georgia 13'))
@@ -61,6 +49,7 @@ class App(tk.Tk):
         self.mainloop()
         return(self.filename.name)
   
-nap=App()
-ResumeFolder=nap.getPath()
-print(ResumeFolder)
+#  FOR TESTING
+# nap=App()
+# ResumeFolder=nap.getDirectoryPath()
+# print(ResumeFolder)
